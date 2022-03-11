fcf25c9d14435ae43fca2eba5c4c9cef2f7e27d1
seokjunyeom <seokjun.dev@gmail.com>
2022-03-11 12:58:20 +0900

    [feat] FILE_MUSIC_ROOT_DIRECTORY 수정

--- settings/base.py
+++ settings/base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from pydantic import BaseSettings
@@ -5,4 +6,12 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-    FILE_MUSIC_ROOT_DIRECTORY = Path('C:\\Users\\tjrwn\\Music')
+
+    if _FILE_MUSIC_ROOT_DIRECTORY := os.environ.get('FILE_MUSIC_ROOT_DIRECTORY'):
+        pass
+    elif user_profile := os.environ.get('USERPROFILE'):
+        _FILE_MUSIC_ROOT_DIRECTORY = Path(user_profile) / Path('music')
+    else:
+        _FILE_MUSIC_ROOT_DIRECTORY = ''
+
+    FILE_MUSIC_ROOT_DIRECTORY = _FILE_MUSIC_ROOT_DIRECTORY
