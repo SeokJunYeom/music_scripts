7f5d9bda24ed779f53cab82c2a2fddf83a855cc2
seokjunyeom <seokjun.dev@gmail.com>
2022-03-15 09:05:36 +0900

    [feat] rename _cover to cover

--- domain/repository/i_backup_repository.py
+++ domain/repository/i_backup_repository.py
@@ -1,5 +1,11 @@
 import abc
+from typing import Generator
+
+from domain.entity.music_entity import MusicEntity
 
 
 class IBackupRepository(metaclass=abc.ABCMeta):
-    pass
+
+    @abc.abstractmethod
+    def save_all(self, musics: Generator[MusicEntity, None, None]):
+        raise NotImplemented
--- tests/infrastructure/file/repositories/test_file_music_repository.py
+++ tests/infrastructure/file/repositories/test_file_music_repository.py
@@ -3,12 +3,32 @@ from types import GeneratorType
 from infrastructure.file.repository.file_music_repository import FileMusicRepository
 
 
-def test_get_all_musics(music_entities):
+def test_check_type_music_entities():
     repository = FileMusicRepository()
     musics = repository.get_all_musics()
 
+    assert isinstance(musics, GeneratorType)
+
+
+def test_get_all_musics_when_success(music_entities):
+    repository = FileMusicRepository()
+    musics = repository.get_all_musics()
     list_music_entities = list(music_entities)
 
-    assert isinstance(musics, GeneratorType)
     for music in musics:
         assert music in list_music_entities
+
+
+def test_get_all_musics_when_fail(music_entities):
+    repository = FileMusicRepository()
+    musics = repository.get_all_musics()
+    list_music_entities = list(music_entities)
+    list_music_entities.pop()
+
+    count = 0
+
+    for music in musics:
+        if music not in list_music_entities:
+            count += 1
+
+    assert count >= 1
